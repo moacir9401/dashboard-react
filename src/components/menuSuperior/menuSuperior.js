af3329bdb376915c0e7cd6f6a9519af6910ff835
Moacir <moacir9401@gmail.com>
2019-07-23 13:32:17 -0300

    inicial

---
+++ src/components/menuSuperior/menuSuperior.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const menuSuperior = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a href="/" className="navbar-brand">Meu Dasboard</a>
+    <button className="navbar-toggler" type="button" data-togger="collapse" data-target="#navbarNav">
+        <span className="navbar-togger-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+            <li>sa</li>
+        </ul>
+    </div>
+</nav>
+);
+
+
+export default menuSuperior;
